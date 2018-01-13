217eb7f69e8a92f7dd66e52b5db9bf5e2f0c476f
Eduard <edo.terakyan@gmail.com>
2018-01-13 22:11:33 +0400

    currecny

--- src/views/settings/general.blade.php
+++ src/views/settings/general.blade.php
@@ -1,7 +1,33 @@
 @extends('btybug::layouts.mTabs',['index'=>'payment_settings'])
 <!-- Nav tabs -->
 @section('tab')
-    general
+    <div class="container-fluid">
+        <h2>General options</h2>
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab">
+                    <h4 class="panel-title">Defaults</h4>
+                </div>
+                <div class="panel-body">
+                    {!! Form::model(null,[]) !!}
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-12 m-b-10">
+                                <div class="col-sm-4 p-l-0">Currency</div>
+                                {!! Form::select('currency',['' => 'Select Currency','usd' => 'USD','gbp' => 'GBP','amd' => 'AMD'],['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::submit("Save",['class' => 'btn settingBtn pull-right']) !!}
+                    </div>
+                    {!! Form::close() !!}
+
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 @section('CSS')
 @stop
