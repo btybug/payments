fe2df8617f611536b11aedf268bfed0b05b8c04d
terakyan <edo.terakyan@gmail.com>
2018-01-14 22:21:31 -0800

    price

--- src/views/settings/price.blade.php
+++ src/views/settings/price.blade.php
@@ -8,6 +8,7 @@
                 <div class="panel-heading" role="tab">
                     <h4 class="panel-title">simple price</h4>
                     <span class="pull-right">
+                        <a href="">See From</a>
                         Active {!! Form::checkbox('simple_price',1) !!}
                     </span>
                 </div>
@@ -19,7 +20,20 @@
                 <div class="panel-heading" role="tab" >
                     <h4 class="panel-title">Price plan</h4>
                     <span class="pull-right">
+                        <a href="">See From</a>
                         Active {!! Form::checkbox('price_plan',1) !!}
+                    </span>
+                </div>
+                <div class="panel-body">
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" >
+                    <h4 class="panel-title">Attributes based price</h4>
+                    <span class="pull-right">
+                        <a href="">See From</a>
+                        Active {!! Form::checkbox('price_attributtes',1) !!}
                     </span>
                 </div>
                 <div class="panel-body">
