57df4359e78d9068680a77f967cbc93f94dc1f5c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-15 15:01:59 +0400

    product single page ui

--- src/Units/unit_for_post_from_membership/tpl.blade.php
+++ src/Units/unit_for_post_from_membership/tpl.blade.php
@@ -1,79 +1,126 @@
-@php
-    $membershipTypesRepository=new \BtyBugHook\Membership\Repository\PlansRepository();
-    $plans=$membershipTypesRepository->findAllByMultiple(['is_active'=>1]);
-@endphp
-@if((!isset($settings['grid_system']) || $settings['grid_system']!=2 ) || (isset($settings['grid_system']) && $settings['grid_system']==1 ))
-    <section id="starter">
+<section class="product-single-head">
     <div class="container">
         <div class="row">
-            @foreach($plans as $plan)
-            <div class="col-sm-4 block">
-                <div class="block-black text-center">
-                    <div class="title">
-                        {!! $plan->name !!}
+            <div class="col-md-6 col-sm-6 col-xs-12 left-product">
+                <div class="img">
+                    <img src="https://ak1.ostkcdn.com/images/products/8818677/Samsung-Galaxy-S4-I337-16GB-AT-T-Unlocked-GSM-Android-Cell-Phone-85e3430e-6981-4252-a984-245862302c78_600.jpg"
+                         alt="">
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12 right-product">
+                <div class="product-title">
+                    <h1>SAMSUNG GALAXY S4 I337 16GB 4G LTE UNLOCKED GSM ANDROID CELL PHONE</h1>
+                </div>
+                <div class="product-name">
+                    <p>vendido por <a href="#">Samsung</a> ·
+                        <small>(5054 ventas)</small>
+                    </p>
+                </div>
+                <div class="product-price">
+                    <span>$399</span>
+                </div>
+                <div class="product-add-form">
+                    <form>
+                        <fieldset class="color">
+                            <div>
+                                <h6>COLOR</h6>
+                                <label class="gray"><input name="product-color" type="radio"><span></span></label>
+                                <label class="black"><input name="product-color" type="radio"><span></span></label>
+                            </div>
+                        </fieldset>
+                        <fieldset class="size">
+                            <div>
+                                <h6>CAPACIDAD</h6>
+                                <label><input name="product-size" type="radio"><span>16 GB</span></label>
+                                <label><input name="product-size" type="radio"><span>32 GB</span></label>
+                            </div>
+                        </fieldset>
+                        <fieldset class="add-count">
+                            <div class="count">
+                                <h6>CANTIDAD</h6>
+                                <input type="number" value="1" min="1">
+                            </div>
+                            <div class="buy-product">
+                                <button><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+                                    Agregar
+                                    al carro
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+                <div class="add-list">
+                    <a href="#"><span class="glyphicon glyphicon-heart-empty"></span> Agregar a lista de deseos</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="product-single-detail">
+    <div class="container">
+        <div class="row">
+            <div class="product_tabs">
+                <ul class="nav nav-tabs">
+                    <li class="">
+                        <a href="#tab-description" data-toggle="tab" aria-expanded="false">
+                            DETALLE DEL PRODUCTO
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#tab-specification" data-toggle="tab" aria-expanded="false">
+                            GARANTÍA
+                        </a>
+                    </li>
+
+                    <li class="active">
+                        <a href="#tab-review" data-toggle="tab" aria-expanded="true">
+                            VENDEDOR
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#tab-shipping" data-toggle="tab" aria-expanded="false">
+                            ENVÍO
+                        </a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tab-description">
+                        <p>STAY CONNECTED EITHER ON THE PHONE OR THE WEB WITH THE GALAXY S4 I337 FROM SAMSUNG. WITH 16 GB OF MEMORY AND A 4G CONNECTION, THIS PHONE STORES PRECIOUS PHOTOS AND VIDEO AND LETS YOU UPLOAD THEM TO A CLOUD OR SOCIAL NETWORK AT BLINDING-FAST SPEED. WITH A 17-HOUR OPERATING LIFE FROM ONE CHARGE, THIS PHONE ALLOWS YOU KEEP IN TOUCH EVEN ON THE GO. WITH ITS BUILT-IN PHOTO EDITOR, THE GALAXY S4 ALLOWS YOU TO EDIT PHOTOS WITH THE TOUCH OF A FINGER, ELIMINATING EXTRANEOUS BACKGROUND ITEMS. USABLE WITH MOST CARRIERS, THIS SMARTPHONE IS THE PERFECT COMPANION FOR WORK OR ENTERTAINMENT.</p>
                     </div>
-                    <div class="header-content text-center">
-                        <b> {!! $plan->amount.' '.$plan->currency !!}</b><br>
-                        <small>{!! $plan->interval.'ly' !!}</small>
-                    </div>
-                    <div class="block-content">
-                        <ul class="list-unstyled">
-                            <li>Full access</li>
-                            <li>Documentation</li>
-                            <li>Costumers support</li>
-                            <li>Free updates</li>
-                            <li>Unlimited domains</li>
+                    <div class="tab-pane" id="tab-specification">
+                        <ul>
+                            <li>Super AMOLED capacitive touchscreen display with 16M colors</li>
+                            <li>Available on GSM, AT T, T-Mobile and other carriers</li>
+                            <li>Compatible with GSM 850 / 900 / 1800; HSDPA 850 / 1900 / 2100 LTE; 700 MHz Class 17 / 1700 / 2100 networks</li>
+                            <li>MicroUSB and USB connectivity</li>
+                            <li>Interfaces with Wi-Fi 802.11 a/b/g/n/ac, dual band and Bluetooth</li>
+                            <li>Wi-Fi hotspot to keep other devices online when a connection is not available</li>
+                            <li>SMS, MMS, email, Push Mail, IM and RSS messaging</li>
+                            <li>Front-facing camera features autofocus, an LED flash, dual video call capability and a sharp 4128 x 3096 pixel picture</li>
+                            <li>Features 16 GB memory and 2 GB RAM</li>
+                            <li>Upgradeable Jelly Bean v4.2.2 to Jelly Bean v4.3 Android OS</li>
+                            <li>17 hours of talk time, 350 hours standby time on one charge</li>
+                            <li>Available in white or black</li>
+                            <li>Model I337</li>
+                            <li>Package includes phone, charger, battery and user manual</li>
+                            <li>Phone is 5.38 inches high x 2.75 inches wide x 0.13 inches deep and weighs a mere 4.59 oz </li>
                         </ul>
                     </div>
-                    <div class="text-center">
-                        <button class="btn select-plan">Add To Cart</button>
-                        <a href="{!! url('product',$plan->id) !!}" class="btn select-plan">View Product</a>
+                    <div class="tab-pane" id="tab-review">
+
+                    </div>
+                    <div id="tab-shipping" class="tab-pane">
+                        <div>
+
+                        </div>
+                        <div class="clearfix"></div>
                     </div>
                 </div>
             </div>
-                @endforeach
         </div>
     </div>
 </section>
 
-{!! BBstyle($_this->path."/css/main1.css") !!}
-@else
-<section id="hosting-content">
-    <div class="container">
-        <div class="row">
-            @foreach($plans as $plan)
-                <div class="col-sm-4 block">
-                    <div class="block-white text-center">
-                        <hr>
-                        <div class="block-title">
-                            {!! $plan->name !!}
-                        </div>
-                        <div class="block-content">
-                            <ul class="list-unstyled">
-                                <li><b>{!! $plan->interval.'ly' !!} </b> Interval</li>
-                                <li><b>{!! $plan->interval_count !!}</b> Interval Count</li>
-                                <li><b>100tb</b> transfer</li>
-                                <li><b>200</b> address e-mail</li>
-                            </ul>
-                        </div>
-                        <div class="block-footer">
-                            <b>${!! $plan->amount !!}</b>
-                            <sup><b>00</b></sup>
-                            <sub>{!! $plan->intrval_count.' '.$plan->interval !!}</sub>
-                        </div>
-                        <a href="#" class="buy btn">
-                            Add To Cart
-                        </a>
-                        <a href="{!! url('product',$plan->id) !!}" class="buy btn">
-                            View Product
-                        </a>
-                    </div>
-                </div>
-            @endforeach
-
-        </div>
-    </div>
-</section>
 {!! BBstyle($_this->path."/css/main2.css") !!}
 {!!  BBscript($_this->path.'/js/main.js') !!}
-    @endif
