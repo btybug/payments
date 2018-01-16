3a1b768ddef67f9a80335bc6b3069c8ecf1a7e6b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-16 15:54:48 +0400

    div ui

--- src/Units/unit_for_post_from_membership/tpl.blade.php
+++ src/Units/unit_for_post_from_membership/tpl.blade.php
@@ -1,104 +1,136 @@
-<section class="product-single-head">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-sm-6 col-xs-12 left-product">
-                @if(isset($settings["area1"]))
-                    <div class="img">
-                        {{-- https://ak1.ostkcdn.com/images/products/8818677/Samsung-Galaxy-S4-I337-16GB-AT-T-Unlocked-GSM-Android-Cell-Phone-85e3430e-6981-4252-a984-245862302c78_600.jpg --}}
-                        <img src="{{$settings["area1"]}}" alt="">
+<div class="container all-div">
+    <div class="row">
+        <div class="head">
+            <div class="col-md-5 col-sm-5 col-xs-12 opt1">
+                Option 1
+            </div>
+            <div class="col-md-7 col-sm-7 col-xs-12 right">
+                <div class="opt2">Option 2</div>
+                <div class="opt3">Option 3</div>
+                <div class="unit1">Unit 1</div>
+                <div class="unit2">Unit 2</div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="bottom">
+            <div class="in-bottom">
+                <div class="col-md-5 col-sm-5 col-xs-12 opt4">
+                    <div>
+                        Option 4
                     </div>
-                @endif
+                </div>
+                <div class="col-md-7 col-sm-7 col-xs-12 opt5">
+                    <div>
+                        Option 5
+                    </div>
+                </div>
+                <div class="clearfix"></div>
             </div>
-            <div class="col-md-6 col-sm-6 col-xs-12 right-product">
-                <div class="product-title">
-                    @if(isset($settings["area2"]))
-                        <h1>{{$settings["area2"]}}</h1>
-                    @endif
-                </div>
-                <div class="product-name">
-                    @if(isset($settings["area3"]))
-                        <p>{{$settings["area3"]}}</p>
-                    @endif
-                </div>
-
-
-                <div class="product-add-form">
-                    <form>
-                        @if(isset($settings["area4"]))
-                            {!! BBRenderUnits($settings['area4']) !!}
-                        @endif
-                        {{--<div class="product-price">
-                            <span>$399</span>
-                        </div>
-                        <fieldset class="color">
-                            <div>
-                                <h6>COLOR</h6>
-                                <label class="gray"><input name="product-color" type="radio"><span></span></label>
-                                <label class="black"><input name="product-color" type="radio"><span></span></label>
-                            </div>
-                        </fieldset>
-                        <fieldset class="size">
-                            <div>
-                                <h6>CAPACIDAD</h6>
-                                <label><input name="product-size" type="radio"><span>16 GB</span></label>
-                                <label><input name="product-size" type="radio"><span>32 GB</span></label>
-                            </div>
-                        </fieldset>
-                        <fieldset class="add-count">
-                            <div class="count">
-                                <h6>CANTIDAD</h6>
-                                <input type="number" value="1" min="1">
-                            </div>
-                        </fieldset>--}}
-                        <div class="clearfix"></div>
-                        @if(isset($settings["area5"]))
-                            {!! BBRenderUnits($settings['area5']) !!}
-                        @endif
-                        {{--<fieldset class="add-count">
-                            <div class="buy-product">
-                                <button><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
-                                    Agregar
-                                    al carro
-                                </button>
-                            </div>
-                        </fieldset>
-                        <div class="add-list">
-                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span> Agregar a lista de deseos</a>
-                        </div>--}}
-                    </form>
-                </div>
-            </div>
+            <div class="clearfix"></div>
         </div>
     </div>
-</section>
-<section class="product-single-detail">
-    <div class="container">
-        <div class="row">
-            <div class="product_tabs">
-                @if(isset($settings["area6"]) && count($settings["area6"]["titles"]))
-                    <ul class="nav nav-tabs">
-                        @foreach($settings["area6"]["titles"] as $uniq_key => $title)
-                            <li class="{{$loop->first ? 'active' : ''}}">
-                                <a href="#tab-{{$uniq_key}}" data-toggle="tab" aria-expanded="false">
-                                    {{$title}}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                @endif
-                <div class="tab-content">
-                    @if(isset($settings["area6"]) && count($settings["area6"]["descriptions"]))
-                        @foreach($settings["area6"]["descriptions"] as $key => $description)
-                            <div class="{{$loop->first ? 'tab-pane active' : 'tab-pane'}}" id="tab-{{$key}}">
-                                <p>{{$description}}</p>
-                            </div>
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+</div>
+{{--<section class="product-single-head">--}}
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-md-6 col-sm-6 col-xs-12 left-product">--}}
+                {{--@if(isset($settings["area1"]))--}}
+                    {{--<div class="img">--}}
+                        {{-- https://ak1.ostkcdn.com/images/products/8818677/Samsung-Galaxy-S4-I337-16GB-AT-T-Unlocked-GSM-Android-Cell-Phone-85e3430e-6981-4252-a984-245862302c78_600.jpg --}}
+                        {{--<img src="{{$settings["area1"]}}" alt="">--}}
+                    {{--</div>--}}
+                {{--@endif--}}
+            {{--</div>--}}
+            {{--<div class="col-md-6 col-sm-6 col-xs-12 right-product">--}}
+                {{--<div class="product-title">--}}
+                    {{--@if(isset($settings["area2"]))--}}
+                        {{--<h1>{{$settings["area2"]}}</h1>--}}
+                    {{--@endif--}}
+                {{--</div>--}}
+                {{--<div class="product-name">--}}
+                    {{--@if(isset($settings["area3"]))--}}
+                        {{--<p>{{$settings["area3"]}}</p>--}}
+                    {{--@endif--}}
+                {{--</div>--}}
+
+
+                {{--<div class="product-add-form">--}}
+                    {{--<form>--}}
+                        {{--@if(isset($settings["area4"]))--}}
+                            {{--{!! BBRenderUnits($settings['area4']) !!}--}}
+                        {{--@endif--}}
+                        {{--<div class="product-price">--}}
+                            {{--<span>$399</span>--}}
+                        {{--</div>--}}
+                        {{--<fieldset class="color">--}}
+                            {{--<div>--}}
+                                {{--<h6>COLOR</h6>--}}
+                                {{--<label class="gray"><input name="product-color" type="radio"><span></span></label>--}}
+                                {{--<label class="black"><input name="product-color" type="radio"><span></span></label>--}}
+                            {{--</div>--}}
+                        {{--</fieldset>--}}
+                        {{--<fieldset class="size">--}}
+                            {{--<div>--}}
+                                {{--<h6>CAPACIDAD</h6>--}}
+                                {{--<label><input name="product-size" type="radio"><span>16 GB</span></label>--}}
+                                {{--<label><input name="product-size" type="radio"><span>32 GB</span></label>--}}
+                            {{--</div>--}}
+                        {{--</fieldset>--}}
+                        {{--<fieldset class="add-count">--}}
+                            {{--<div class="count">--}}
+                                {{--<h6>CANTIDAD</h6>--}}
+                                {{--<input type="number" value="1" min="1">--}}
+                            {{--</div>--}}
+                        {{--</fieldset>--}}
+                        {{--<div class="clearfix"></div>--}}
+                        {{--@if(isset($settings["area5"]))--}}
+                            {{--{!! BBRenderUnits($settings['area5']) !!}--}}
+                        {{--@endif--}}
+                        {{--<fieldset class="add-count">--}}
+                            {{--<div class="buy-product">--}}
+                                {{--<button><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>--}}
+                                    {{--Agregar--}}
+                                    {{--al carro--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--</fieldset>--}}
+                        {{--<div class="add-list">--}}
+                            {{--<a href="#"><span class="glyphicon glyphicon-heart-empty"></span> Agregar a lista de deseos</a>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</section>--}}
+{{--<section class="product-single-detail">--}}
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<div class="product_tabs">--}}
+                {{--@if(isset($settings["area6"]) && count($settings["area6"]["titles"]))--}}
+                    {{--<ul class="nav nav-tabs">--}}
+                        {{--@foreach($settings["area6"]["titles"] as $uniq_key => $title)--}}
+                            {{--<li class="{{$loop->first ? 'active' : ''}}">--}}
+                                {{--<a href="#tab-{{$uniq_key}}" data-toggle="tab" aria-expanded="false">--}}
+                                    {{--{{$title}}--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                        {{--@endforeach--}}
+                    {{--</ul>--}}
+                {{--@endif--}}
+                {{--<div class="tab-content">--}}
+                    {{--@if(isset($settings["area6"]) && count($settings["area6"]["descriptions"]))--}}
+                        {{--@foreach($settings["area6"]["descriptions"] as $key => $description)--}}
+                            {{--<div class="{{$loop->first ? 'tab-pane active' : 'tab-pane'}}" id="tab-{{$key}}">--}}
+                                {{--<p>{{$description}}</p>--}}
+                            {{--</div>--}}
+                        {{--@endforeach--}}
+                    {{--@endif--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</section>--}}
 
 {!! BBstyle($_this->path."/css/main2.css") !!}
 {!!  BBscript($_this->path.'/js/main.js') !!}
