987a52eb5c4f07765f7403a7b15c7c9121d1eca0
terakyan <edo.terakyan@gmail.com>
2018-01-16 07:08:11 -0800

    price

--- src/views/settings/price/_partials/settings/quantity_price.blade.php
+++ src/views/settings/price/_partials/settings/quantity_price.blade.php
@@ -2,61 +2,48 @@
     $data = get_qty_data();
 @endphp
 <div class="col-md-12">
-    {!! Form::model($data,['route' => 'payments_settings_qty_save','class' => 'form-horizontal']) !!}
     <div class="col-md-12 qty-box">
-        @if(isset($data['qty']) && count($data['qty']))
-            @foreach($data['qty'] as $key => $val)
-                <div class="row">
-                    <div class="col-md-6">
-                        <label>
-                            Quantity :
-                        </label>
-                        <input type="text" class="form-control" value="{{ $val['qty'] }}" name="qty[{{$key}}][qty]"/>
-                    </div>
-                    <div class="col-md-6">
-                        <label>
-                            Price :
-                        </label>
-                        <input type="text" class="form-control" name="qty[{{$key}}][price]"/>
-                    </div>
-                </div>
-            @endforeach
-        @else
-            <div class="row">
-                <div class="col-md-6">
-                    <label>
-                        Quantity :
-                    </label>
-                    <input type="text" class="form-control" name="qty[0][qty]"/>
-                </div>
-                <div class="col-md-6">
-                    <label>
-                        Price :
-                    </label>
-                    <input type="text" class="form-control" name="qty[0][price]"/>
-                </div>
+        <div class="row">
+            <div class="col-md-6">
+                <label>
+                    Quantity :
+                </label>
+                <input type="text" class="form-control" name="qty[0][qty]"/>
             </div>
-        @endif
+            <div class="col-md-6">
+                <label>
+                    Price :
+                </label>
+                <input type="text" class="form-control" name="qty[0][price]"/>
+            </div>
+        </div>
     </div>
-
     <div class="col-md-12">
         <a href="javascript:void(0)" class="add-new-qty"><i class="fa fa-plus"></i> add new</a>
     </div>
-
-    <div class="col-md-12 m-t-15">
-        <div class="col-md-4">
-            Display result as
+    
+    {!! Form::model($data,['route' => 'payments_settings_qty_save','class' => 'form-horizontal']) !!}
+        <div class="col-md-12 m-t-15">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab">
+                    <h4 class="panel-title">
+                        Settings
+                        {!! Form::submit('save',['class' => 'btn btn-primary pull-right']) !!}
+                    </h4>
+                </div>
+                <div class="panel-body">
+                    <div class="col-md-4">
+                        Display result as
+                    </div>
+                    <div class="col-md-4">
+                        {!! Form::radio('qty_option','select',true) !!} Select menu
+                    </div>
+                    <div class="col-md-4">
+                        {!! Form::radio('qty_option','radio',null) !!} Radio
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4">
-            {!! Form::radio('qty_option','select',true) !!} Select menu
-        </div>
-        <div class="col-md-4">
-            {!! Form::radio('qty_option','radio',null) !!} Radio
-        </div>
-    </div>
-    <div class="col-md-12">
-        {!! Form::submit('save',['class' => 'btn btn-primary']) !!}
-    </div>
     {!! Form::close() !!}
 </div>
 
