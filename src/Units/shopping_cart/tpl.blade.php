8ac75b253b1090f84bed3157357e5c71d9458b35
terakyan <edo.terakyan@gmail.com>
2018-01-23 02:45:41 -0800

    shopping cart

--- src/Units/shopping_cart/tpl.blade.php
+++ src/Units/shopping_cart/tpl.blade.php
@@ -1,3 +1,7 @@
+@php
+use Gloudemans\Shoppingcart\Facades\Cart;
+@endphp
+
 <div class="container">
     <div class="row">
         <h1>Shopping Cart</h1>
@@ -12,63 +16,46 @@
                 <label class="product-removal">Remove</label>
                 <label class="product-line-price">Total</label>
             </div>
-
-            <div class="product">
-                <div class="product-image">
-                    <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg" alt="" class="{{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : ''}}">
-                </div>
-                <div class="product-details">
-                    <div class="product-title">
-                        <p class="{{isset($settings['option_2_container_item_style']) ? $settings['option_2_container_item_style'] : ''}} {{isset($settings['option_3_container_item_style']) ? $settings['option_3_container_item_style'] : ''}}">
-                            Dingo Dog Bones
-                        </p>
+            @if(Cart::count())
+                @foreach(Cart::content() as $item)
+                    <div class="product">
+                        <div class="product-image">
+                            <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg" alt="" class="{{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : ''}}">
+                        </div>
+                        <div class="product-details">
+                            <div class="product-title">
+                                <p class="{{isset($settings['option_2_container_item_style']) ? $settings['option_2_container_item_style'] : ''}} {{isset($settings['option_3_container_item_style']) ? $settings['option_3_container_item_style'] : ''}}">
+                                   {!! $item->name !!}
+                                </p>
+                            </div>
+                            <p class="product-description {{isset($settings['option_4_container_item_style']) ? $settings['option_4_container_item_style'] : ''}} {{isset($settings['option_5_container_item_style']) ? $settings['option_5_container_item_style'] : ''}}">
+                                The best dog bones of all time. Holy crap. Your dog will be begging
+                                for these things! I got curious once and ate one myself. I'm a fan.</p>
+                        </div>
+                        <div class="product-price {{isset($settings['option_6_container_item_style']) ? $settings['option_6_container_item_style'] : ''}} {{isset($settings['option_7_container_item_style']) ? $settings['option_7_container_item_style'] : ''}}">
+                            {!! $item->price !!}
+                        </div>
+                        <div class="product-quantity">
+                            <input type="number" value="{!! $item->qty !!}" min="1">
+                        </div>
+                        <div class="product-removal">
+                            <button class="{{isset($settings['option_8_container_item_style']) ? $settings['option_8_container_item_style'] : ''}} {{isset($settings['option_9_container_item_style']) ? $settings['option_9_container_item_style'] : ''}}">
+                                Remove
+                            </button>
+                        </div>
+                        <div class="product-line-price">{!! $item->price !!}</div>
                     </div>
-                    <p class="product-description {{isset($settings['option_4_container_item_style']) ? $settings['option_4_container_item_style'] : ''}} {{isset($settings['option_5_container_item_style']) ? $settings['option_5_container_item_style'] : ''}}">The best dog bones of all time. Holy crap. Your dog will be begging
-                        for these things! I got curious once and ate one myself. I'm a fan.</p>
-                </div>
-                <div class="product-price {{isset($settings['option_6_container_item_style']) ? $settings['option_6_container_item_style'] : ''}} {{isset($settings['option_7_container_item_style']) ? $settings['option_7_container_item_style'] : ''}}">12.99</div>
-                <div class="product-quantity">
-                    <input type="number" value="2" min="1">
-                </div>
-                <div class="product-removal">
-                    <button class="{{isset($settings['option_8_container_item_style']) ? $settings['option_8_container_item_style'] : ''}} {{isset($settings['option_9_container_item_style']) ? $settings['option_9_container_item_style'] : ''}}">
-                        Remove
-                    </button>
-                </div>
-                <div class="product-line-price">25.98</div>
-            </div>
-
-            <div class="product">
-                <div class="product-image">
-                    <img src="https://s.cdpn.io/3/large-NutroNaturalChoiceAdultLambMealandRiceDryDogFood.png" class="{{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : ''}}">
-                </div>
-                <div class="product-details">
-                    <div class="product-title">
-                        <p class="{{isset($settings['option_2_container_item_style']) ? $settings['option_2_container_item_style'] : ''}} {{isset($settings['option_3_container_item_style']) ? $settings['option_3_container_item_style'] : ''}}">Nutro™ Adult Lamb and Rice Dog Food</p>
-                    </div>
-                    <p class="product-description {{isset($settings['option_4_container_item_style']) ? $settings['option_4_container_item_style'] : ''}} {{isset($settings['option_5_container_item_style']) ? $settings['option_5_container_item_style'] : ''}}">Who doesn't like lamb and rice? We've all hit the halal cart at 3am
-                        while quasi-blackout after a night of binge drinking in Manhattan. Now it's your dog's turn!</p>
-                </div>
-                <div class="product-price {{isset($settings['option_6_container_item_style']) ? $settings['option_6_container_item_style'] : ''}} {{isset($settings['option_7_container_item_style']) ? $settings['option_7_container_item_style'] : ''}}">45.99</div>
-                <div class="product-quantity">
-                    <input type="number" value="1" min="1">
-                </div>
-                <div class="product-removal">
-                    <button class="{{isset($settings['option_8_container_item_style']) ? $settings['option_8_container_item_style'] : ''}} {{isset($settings['option_9_container_item_style']) ? $settings['option_9_container_item_style'] : ''}}">
-                        Remove
-                    </button>
-                </div>
-                <div class="product-line-price">45.99</div>
-            </div>
+                @endforeach
+            @endif
 
             <div class="totals">
                 <div class="totals-item">
                     <label>Subtotal</label>
-                    <div class="totals-value" id="cart-subtotal">71.97</div>
+                    <div class="totals-value" id="cart-subtotal">{!! Cart::subtotal() !!}</div>
                 </div>
                 <div class="totals-item">
                     <label>Tax (5%)</label>
-                    <div class="totals-value" id="cart-tax">3.60</div>
+                    <div class="totals-value" id="cart-tax">{!! Cart::tax() !!}</div>
                 </div>
                 <div class="totals-item">
                     <label>Shipping</label>
@@ -76,7 +63,7 @@
                 </div>
                 <div class="totals-item totals-item-total">
                     <label>Grand Total</label>
-                    <div class="totals-value" id="cart-total">90.57</div>
+                    <div class="totals-value" id="cart-total">{!! Cart::total() !!}</div>
                 </div>
             </div>
 
