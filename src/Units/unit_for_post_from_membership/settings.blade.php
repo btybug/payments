3f389a68264e2001f14b6d52c243e848965d8a82
gev2092 <gev2092harutyunyan@gmail.com>
2018-01-22 23:16:55 -0800

    dinamic blog's post

--- src/Units/unit_for_post_from_membership/settings.blade.php
+++ src/Units/unit_for_post_from_membership/settings.blade.php
@@ -1,7 +1,30 @@
 <?php
-$postRepo = new \BtyBugHook\Blog\Repository\PostsRepository();
-$post = $postRepo->first()->toArray();
-$json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
+//$json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
+
+$table = "";
+$columns = [];
+if(isset($settings["table"])){
+    $table = $settings["table"];
+}
+$blogs = BtyBugHook\Membership\Models\Blog::select("title","slug")->get();
+if($table){
+    $columns = \DB::select('SHOW COLUMNS FROM ' . $table);
+}
+
+
+function renderOptions($columns){
+    $html = '';
+    if(count($columns)){
+        foreach($columns as $key => $data){
+            if($data->Field == "image"){
+                continue;
+            }else{
+                $html .= '<option value="'.$data->Field.'">'.$data->Field.'</option>';
+            }
+        }
+    }
+    return $html;
+}
 ?>
 
 <div class="row">
@@ -11,9 +34,13 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
                 <label for="">Select Blog</label>
             </div>
             <div class="col-md-6" style="margin-bottom:15px">
-                <select name="" class="form-control">
+                <select name="table" class="form-control">
                     <option value="">select option</option>
-                    <option value="mobile">mobile</option>
+                    @if(count($blogs))
+                        @foreach($blogs as $blog)
+                            <option value="{{$blog->slug}}">{{$blog->title}}</option>
+                        @endforeach
+                    @endif
                 </select>
             </div>
         </div>
@@ -34,9 +61,15 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
                         <div class="col-md-6">
                             <select name="option_1_item_value" class="form-control">
                                 <option value="">select option</option>
-                                @foreach($json["images"] as $key => $data)
-                                    <option value="{{$data}}">Image {{$key+1}}</option>
-                                @endforeach
+                                @if(count($columns))
+                                    @foreach($columns as $key => $data)
+                                        @if($data->Field == "image")
+                                            <option value="{{$data->Field}}">Image</option>
+                                        @else
+                                            @continue
+                                        @endif
+                                    @endforeach
+                                @endif
                             </select>
                         </div>
                     </div>
@@ -93,12 +126,7 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
                         <div class="col-md-6">
                             <select name="option_2_item_value" class="form-control">
                                 <option value="">select option</option>
-                                @foreach($json as $key => $data)
-                                    @if($key == "images")
-                                        @continue
-                                    @endif
-                                    <option value="{{$data}}">{{$key}}</option>
-                                @endforeach
+                                {!! renderOptions($columns) !!}
                             </select>
                         </div>
                     </div>
@@ -162,12 +190,7 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
                         <div class="col-md-6">
                             <select name="option_3_item_value" class="form-control">
                                 <option value="">select option</option>
-                                @foreach($json as $key => $data)
-                                    @if($key == "images")
-                                        @continue
-                                    @endif
-                                    <option value="{{$data}}">{{$key}}</option>
-                                @endforeach
+                                {!! renderOptions($columns) !!}
                             </select>
                         </div>
                     </div>
@@ -272,69 +295,6 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
                     </div>
                 </div>
             </div>
-
-
-            {{--<div id="collapse5" class="collapse in" aria-expanded="true" style="">
-                <div class="content">
-                    <div class="form-group">
-                        <div class="col-md-6">
-                            <label for="">Item</label>
-                        </div>
-                        <div class="col-md-6">
-                            <select name="option_4_item_value" class="form-control">
-                                <option value="">select option</option>
-                                @foreach($json as $key => $data)
-                                    @if($key == "images")
-                                        @continue
-                                    @endif
-                                    <option value="{{$data}}">{{$key}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-6">
-                            <label for="">Container style</label>
-                        </div>
-                        <div class="col-md-6">
-                            <select name="option_4_container_style" class="form-control">
-                                <option value="">select option</option>
-                                <option value="option-1">bg-brown, p-15, border-1, shadow</option>
-                                <option value="option-2">bg-lightblue, p-15, border-1, shadow</option>
-                                <option value="option-3">bg-gray, p-15, border-1</option>
-                                <option value="option-4">bg-lightviolet, p-20</option>
-                                <option value="option-5">bg-lightgreen, p-20, radius-4</option>
-                                <option value="option-6">bg-lightred, p-24, radius-8, shadow</option>
-                                <option value="option-7">bg-white, p-24, radius-8, shadow</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-6">
-                            <label for="">Item style</label>
-                        </div>
-                        <div class="col-md-6">
-                            <select name="option_4_container_item_style" class="form-control">
-                                <option value="">select option</option>
-                                <option value="font-1">font 16px</option>
-                                <option value="font-2">16px bold</option>
-                                <option value="font-3">18px</option>
-                                <option value="font-4">18px bold</option>
-                                <option value="font-5">20px bold</option>
-                                <option value="font-6">16px white</option>
-                                <option value="font-7">16px white-bold</option>
-                                <option value="font-8">18px white-bold</option>
-                                <option value="font-9">16px red</option>
-                                <option value="font-10">18px red-bold</option>
-                                <option value="font-11">20px red-bold</option>
-                                <option value="font-12">16px blue</option>
-                                <option value="font-13">18px blue-bold</option>
-                                <option value="font-14">20px blue-bold</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-            </div>--}}
         </div>
         <div class="bty-panel-collapse">
             <div>
@@ -352,12 +312,7 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
                         <div class="col-md-6">
                             <select name="option_5_item_value" class="form-control">
                                 <option value="">select option</option>
-                                @foreach($json as $key => $data)
-                                    @if($key == "images")
-                                        @continue
-                                    @endif
-                                    <option value="{{$data}}">{{$key}}</option>
-                                @endforeach
+                                {!! renderOptions($columns) !!}
                             </select>
                         </div>
                     </div>
@@ -407,94 +362,6 @@ $json = json_decode(file_get_contents($_this->path.DS."db.json"), true);
         </div>
     </div>
 </div>
-
-{{--<div class="row">
-    <div class="col-xs-12 ">
-        <div class="form-group">
-            <label for="area1">Image</label>
-            <input type="text" class="form-control" placeholder="Image link" value="{{(isset($settings['area1']) ? $settings['area1'] : '')}}" name="area1">
-        </div>
-        <div class="form-group">
-            <label for="area1">Title</label>
-            --}}{{--<select name="area2" id="area2" class="form-control">
-                <option value="">select option</option>
-                @foreach($post as $key => $value)
-                    <option value="{{$key}}" {{(isset($settings["area2"]) && $settings["area2"] === $key) ? 'selected' : ''}}>{{$key}}</option>
-                @endforeach
-            </select>--}}{{--
-            <input type="text" class="form-control" placeholder="Title" value="{{(isset($settings['area2']) ? $settings['area2'] : '')}}" name="area2">
-        </div>
-        <div class="form-group">
-            <label for="area3">Sub title</label>
-            <input type="text" class="form-control" placeholder="Sub Title" value="{{(isset($settings['area3']) ? $settings['area3'] : '')}}" name="area3">
-        </div>
-        <div class="form-group">
-            <label for="area4">Price and details</label>
-            <div class="form-group my_rows1">
-                <div class="col-sm-8">
-                    {!! BBbutton2('unit',"area4","product","Change",['class'=>'btn btn-default change-layout','data-type'=>'frontend_sidebar','model'=>$settings]) !!}
-                </div>
-            </div>
-        </div>
-        <div class="clearfix"></div>
-        <div class="form-group">
-            <label for="area4">Button add to card</label>
-            <div class="form-group my_rows1">
-                <div class="col-sm-8">
-                    {!! BBbutton2('unit',"area5","product","Change",['class'=>'btn btn-default change-layout','data-type'=>'frontend_sidebar','model'=>$settings]) !!}
-                </div>
-            </div>
-            --}}{{--<div class="form-group my_rows1 custom_show {{$settings['is_unit_area_5']?'':'custom_hidden'}}">
-                <div class="col-sm-8">
-                    {!! BBbutton2('unit',"area5","product","Change",['class'=>'btn btn-default change-layout','data-type'=>'frontend_sidebar','model'=>$settings]) !!}
-                </div>
-            </div>
-            <select name="area5" id="area5" class="form-control custom_remove {{isset($settings['is_unit_area_5']) && $settings['is_unit_area_5']==1?'custom_hidden':''}}">
-                <option value="">select option</option>
-                @foreach($post as $key => $value)
-                    <option value="{{$key}}" {{(isset($settings["area5"]) && $settings["area5"] === $key) ? 'selected' : ''}}>{{$key}}</option>
-                @endforeach
-            </select>
-            <div>
-                <p>Is Unit</p>
-                yes <input type="radio" value="1" name="is_unit_area_5" class="is_unit" {{ (isset($settings['is_unit_area_5']) && $settings["is_unit_area_5"] === 1) ? 'checked' : '' }}>
-                no <input type="radio" value="0" name="is_unit_area_5" class="is_unit" {{ (isset($settings['is_unit_area_5']) && $settings["is_unit_area_5"] === 0) ? 'checked' : '' }}{{ !isset($settings['is_unit_area_5'])?'checked':"" }}>
-            </div>--}}{{--
-        </div>
-        <div class="clearfix"></div>
-        <div class="form-group">
-            <label for="area4">Description</label>
-            <div class="clearfix"></div>
-            <div class="for_append">
-                @if(isset($settings["area6"]["titles"]))
-                    @foreach($settings["area6"]["titles"] as $key => $title)
-                        <div class="single_setting" data-id="{{$key}}">
-                            <label for="area6[titles][{{$key}}]">Title</label>
-                            <input type="text" placeholder="Insert title" name="area6[titles][{{$key}}]" id="area6[titles][{{$key}}]" class="form-control" value="{{$title}}">
-
-                            <label for="area6[descriptions][{{$key}}]">Content</label>
-                            <textarea name="area6[descriptions][{{$key}}]" id="area6[descriptions][{{$key}}]" class="form-control">{{$settings['area6']['descriptions'][$key]}}</textarea>
-                            --}}{{--@if(!$loop->first)
-                                 <button class="btn btn-danger btn-md pull-right remove_single_setting"></button>
-                            @endif--}}{{--
-                        </div>
-                    @endforeach
-                @else
-                    <div class="single_setting" data-id="0">
-                        <label for="area6[titles][0]">Title</label>
-                        <input type="text" placeholder="Insert title" name="area6[titles][0]" id="area6[titles][0]" class="form-control">
-
-                        <label for="area6[descriptions][0]">Content</label>
-                        <textarea name="area6[descriptions][0]" id="area6[descriptions][0]" class="form-control"></textarea>
-                    </div>
-                @endif
-            </div>
-
-            <button class="btn btn-md btn-success custom_add_new_desc margin_top_15">Add new</button>
-        </div>
-    </div>
-</div>--}}
-
 {!!  BBstyle($_this->path.'/css/settings.css') !!}
 {!!  BBstyle($_this->path.'/css/custom.css') !!}
 
--- src/Units/unit_for_post_from_membership/tpl.blade.php
+++ src/Units/unit_for_post_from_membership/tpl.blade.php
@@ -1,4 +1,5 @@
 <?php
+$product = [];
 if(isset($source['_page'])){
     $page=$source['_page'];
     $params = \Request::route()->parameters();
@@ -8,38 +9,30 @@ if(isset($source['_page'])){
     $blog=str_replace('-','_',str_replace_first('single_','',$page->slug));
     $product=DB::table($blog)->find($param);
 }
-
-
+if(isset($settings["table"]) && !count($product)){
+    $table = $settings["table"];
+    $product = DB::table($table)->select("*")->first();
+}
 ?>
-@if(!isset($product))
+@if(!count($product))
 <div class="container all-div">
     <div class="row">
             <div class="head">
-                <div class="col-md-5 col-sm-5 col-xs-12 opt1 {{isset($settings["option_1_container_style"]) ? $settings["option_1_container_style"] : 'final-opt1'}} ">
-                    @if(isset($settings["option_1_item_value"]))
-                        <img src="{{$settings["option_1_item_value"]}}" alt="" class=" {{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : 'final-img'}}">
-                    @endif
+                <div class="col-md-5 col-sm-5 col-xs-12 opt1 final-opt1">
+
                 </div>
                 <div class="col-md-7 col-sm-7 col-xs-12 right">
-                    <div class="opt2 {{isset($settings["option_2_container_style"]) ? $settings["option_2_container_style"] : 'final-opt2'}} ">
-                        <p class="{{isset($settings['option_2_container_item_style']) ? $settings['option_2_container_item_style'] : 'final-title'}} ">
-                            {{isset($settings["option_2_item_value"]) ? $settings["option_2_item_value"] : ''}}
-                        </p>
+                    <div class="opt2 final-opt2">
+
                     </div>
-                    <div class="opt3 {{isset($settings["option_3_container_style"]) ? $settings["option_3_container_style"] : 'final-opt3'}} ">
-                        <p class="{{isset($settings['option_3_container_item_style']) ? $settings['option_3_container_item_style'] : 'final-desc'}} ">
-                            {{isset($settings["option_3_item_value"]) ? $settings["option_3_item_value"] : ''}}
-                        </p>
+                    <div class="opt3 final-opt3">
+
                     </div>
                     <div class="unit1 final-unit1">
-                        @if(isset($settings["unit1"]))
-                            {!! BBRenderUnits($settings['unit1']) !!}
-                        @endif
+
                     </div>
                     <div class="unit2 final-unit2">
-                        @if(isset($settings["unit2"]))
-                            {!! BBRenderUnits($settings['unit2']) !!}
-                        @endif
+
                     </div>
                     <div class="text-center custom_margin_top_30">
                         <button class="btn btn-success btn-lg">Add to cart</button>
@@ -50,29 +43,18 @@ if(isset($source['_page'])){
         <div class="bottom">
             <div class="in-bottom">
                 <div class="col-md-5 col-sm-5 col-xs-12">
-                   {{-- <div class="opt4 {{isset($settings["option_4_container_style"]) ? $settings["option_4_container_style"] : ''}}">
-                        <p class="{{isset($settings['option_4_container_item_style']) ? $settings['option_4_container_item_style'] : ''}}">
-                            {{isset($settings["option_4_item_value"]) ? $settings["option_4_item_value"] : ''}}
-                        </p>
-                    </div>--}}
                     <div class="opt4 final-opt4">
                     <div class="col-md-12">
-                        @if(isset($settings["unit3"]))
-                            {!! BBRenderUnits($settings['unit3']) !!}
-                        @endif
+
                     </div>
                     <div class="col-md-12">
-                        @if(isset($settings["unit4"]))
-                            {!! BBRenderUnits($settings['unit4']) !!}
-                        @endif
+
                     </div>
                     </div>
                 </div>
                 <div class="col-md-7 col-sm-7 col-xs-12">
-                    <div class="opt5 {{isset($settings["option_5_container_style"]) ? $settings["option_5_container_style"] : 'final-opt5'}} ">
-                        <p class="{{isset($settings['option_5_container_item_style']) ? $settings['option_5_container_item_style'] : ''}}">
-                            {{isset($settings["option_5_item_value"]) ? $settings["option_5_item_value"] : ''}}
-                        </p>
+                    <div class="opt5 final-opt5">
+
                     </div>
                 </div>
                 <div class="clearfix"></div>
@@ -82,24 +64,24 @@ if(isset($source['_page'])){
     </div>
 </div>
 @else
-    <p><h2>Title: {!! $product->title !!}</h2></p>
+    <?php $product = collect($product)->toArray();?>
     <div class="container all-div">
         <div class="row">
             <div class="head">
                 <div class="col-md-5 col-sm-5 col-xs-12 opt1 {{isset($settings["option_1_container_style"]) ? $settings["option_1_container_style"] : 'final-opt1'}} ">
                     @if(isset($settings["option_1_item_value"]))
-                        <img src="{{$settings["option_1_item_value"]}}" alt="" class=" {{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : 'final-img'}}">
+                        <img src="{{$product[$settings["option_1_item_value"]]}}" alt="" class=" {{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : 'final-img'}}">
                     @endif
                 </div>
                 <div class="col-md-7 col-sm-7 col-xs-12 right">
                     <div class="opt2 {{isset($settings["option_2_container_style"]) ? $settings["option_2_container_style"] : 'final-opt2'}} ">
                         <p class="{{isset($settings['option_2_container_item_style']) ? $settings['option_2_container_item_style'] : 'final-title'}} ">
-                            {{isset($settings["option_2_item_value"]) ? $settings["option_2_item_value"] : ''}}
+                            {{isset($settings["option_2_item_value"]) ? $product[$settings["option_2_item_value"]] : ''}}
                         </p>
                     </div>
                     <div class="opt3 {{isset($settings["option_3_container_style"]) ? $settings["option_3_container_style"] : 'final-opt3'}} ">
                         <p class="{{isset($settings['option_3_container_item_style']) ? $settings['option_3_container_item_style'] : 'final-desc'}} ">
-                            {{isset($settings["option_3_item_value"]) ? $settings["option_3_item_value"] : ''}}
+                            {{isset($settings["option_3_item_value"]) ? $product[$settings["option_3_item_value"]] : ''}}
                         </p>
                     </div>
                     <div class="unit1 final-unit1">
@@ -121,11 +103,6 @@ if(isset($source['_page'])){
             <div class="bottom">
                 <div class="in-bottom">
                     <div class="col-md-5 col-sm-5 col-xs-12">
-                        {{-- <div class="opt4 {{isset($settings["option_4_container_style"]) ? $settings["option_4_container_style"] : ''}}">
-                             <p class="{{isset($settings['option_4_container_item_style']) ? $settings['option_4_container_item_style'] : ''}}">
-                                 {{isset($settings["option_4_item_value"]) ? $settings["option_4_item_value"] : ''}}
-                             </p>
-                         </div>--}}
                         <div class="opt4 final-opt4">
                             <div class="col-md-12">
                                 @if(isset($settings["unit3"]))
@@ -142,7 +119,7 @@ if(isset($source['_page'])){
                     <div class="col-md-7 col-sm-7 col-xs-12">
                         <div class="opt5 {{isset($settings["option_5_container_style"]) ? $settings["option_5_container_style"] : 'final-opt5'}} ">
                             <p class="{{isset($settings['option_5_container_item_style']) ? $settings['option_5_container_item_style'] : ''}}">
-                                {{isset($settings["option_5_item_value"]) ? $settings["option_5_item_value"] : ''}}
+                                {{isset($settings["option_5_item_value"]) ? $product[$settings["option_5_item_value"]] : ''}}
                             </p>
                         </div>
                     </div>
@@ -152,113 +129,7 @@ if(isset($source['_page'])){
             </div>
         </div>
     </div>
-
-    @endif
-
-
-
-
-{{--<section class="product-single-head">--}}
-{{--<div class="container">--}}
-{{--<div class="row">--}}
-{{--<div class="col-md-6 col-sm-6 col-xs-12 left-product">--}}
-{{--@if(isset($settings["area1"]))--}}
-{{--<div class="img">--}}
- {{--https://ak1.ostkcdn.com/images/products/8818677/Samsung-Galaxy-S4-I337-16GB-AT-T-Unlocked-GSM-Android-Cell-Phone-85e3430e-6981-4252-a984-245862302c78_600.jpg--}}
-{{--<img src="{{$settings["area1"]}}" alt="">--}}
-{{--</div>--}}
-{{--@endif--}}
-{{--</div>--}}
-{{--<div class="col-md-6 col-sm-6 col-xs-12 right-product">--}}
-{{--<div class="product-title">--}}
-{{--@if(isset($settings["area2"]))--}}
-{{--<h1>{{$settings["area2"]}}</h1>--}}
-{{--@endif--}}
-{{--</div>--}}
-{{--<div class="product-name">--}}
-{{--@if(isset($settings["area3"]))--}}
-{{--<p>{{$settings["area3"]}}</p>--}}
-{{--@endif--}}
-{{--</div>--}}
-
-
-{{--<div class="product-add-form">--}}
-{{--<form>--}}
-{{--@if(isset($settings["area4"]))--}}
-{{--{!! BBRenderUnits($settings['area4']) !!}--}}
-{{--@endif--}}
-{{--<div class="product-price">--}}
-{{--<span>$399</span>--}}
-{{--</div>--}}
-{{--<fieldset class="color">--}}
-{{--<div>--}}
-{{--<h6>COLOR</h6>--}}
-{{--<label class="gray"><input name="product-color" type="radio"><span></span></label>--}}
-{{--<label class="black"><input name="product-color" type="radio"><span></span></label>--}}
-{{--</div>--}}
-{{--</fieldset>--}}
-{{--<fieldset class="size">--}}
-{{--<div>--}}
-{{--<h6>CAPACIDAD</h6>--}}
-{{--<label><input name="product-size" type="radio"><span>16 GB</span></label>--}}
-{{--<label><input name="product-size" type="radio"><span>32 GB</span></label>--}}
-{{--</div>--}}
-{{--</fieldset>--}}
-{{--<fieldset class="add-count">--}}
-{{--<div class="count">--}}
-{{--<h6>CANTIDAD</h6>--}}
-{{--<input type="number" value="1" min="1">--}}
-{{--</div>--}}
-{{--</fieldset>--}}
-{{--<div class="clearfix"></div>--}}
-{{--@if(isset($settings["area5"]))--}}
-{{--{!! BBRenderUnits($settings['area5']) !!}--}}
-{{--@endif--}}
-{{--<fieldset class="add-count">--}}
-{{--<div class="buy-product">--}}
-{{--<button><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>--}}
-{{--Agregar--}}
-{{--al carro--}}
-{{--</button>--}}
-{{--</div>--}}
-{{--</fieldset>--}}
-{{--<div class="add-list">--}}
-{{--<a href="#"><span class="glyphicon glyphicon-heart-empty"></span> Agregar a lista de deseos</a>--}}
-{{--</div>--}}
-{{--</form>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</section>--}}
-{{--<section class="product-single-detail">--}}
-{{--<div class="container">--}}
-{{--<div class="row">--}}
-{{--<div class="product_tabs">--}}
-{{--@if(isset($settings["area6"]) && count($settings["area6"]["titles"]))--}}
-{{--<ul class="nav nav-tabs">--}}
-{{--@foreach($settings["area6"]["titles"] as $uniq_key => $title)--}}
-{{--<li class="{{$loop->first ? 'active' : ''}}">--}}
-{{--<a href="#tab-{{$uniq_key}}" data-toggle="tab" aria-expanded="false">--}}
-{{--{{$title}}--}}
-{{--</a>--}}
-{{--</li>--}}
-{{--@endforeach--}}
-{{--</ul>--}}
-{{--@endif--}}
-{{--<div class="tab-content">--}}
-{{--@if(isset($settings["area6"]) && count($settings["area6"]["descriptions"]))--}}
-{{--@foreach($settings["area6"]["descriptions"] as $key => $description)--}}
-{{--<div class="{{$loop->first ? 'tab-pane active' : 'tab-pane'}}" id="tab-{{$key}}">--}}
-{{--<p>{{$description}}</p>--}}
-{{--</div>--}}
-{{--@endforeach--}}
-{{--@endif--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</section>--}}
+@endif
 
 {!! BBstyle($_this->path."/css/main2.css") !!}
 {!!  BBscript($_this->path.'/js/main.js') !!}
