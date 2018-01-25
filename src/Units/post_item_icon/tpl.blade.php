bae271c58fd69a11942f5cdffbdce8f02ffd75ad
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-25 13:56:25 +0400

    product icon ui

--- src/Units/post_item_icon/tpl.blade.php
+++ src/Units/post_item_icon/tpl.blade.php
@@ -8,21 +8,12 @@
                     </div>
 
                     <div class="plan-icon">
-                        <i class="fa fa-adjust" aria-hidden="true"></i>
+                        <img src="http://www.smlclebanon.com/Content/uploads/ProductType/439~product1.jpg" alt="">
+                        {{--<i class="fa fa-adjust" aria-hidden="true"></i>--}}
                     </div>
                     <div class="plan-name">
                         Personal
                     </div>
-
-                    <ul class="plan-details">
-                        <li>1 sub-domain</li>
-                        <li><strong>10</strong> email addresses</li>
-                        <li><strong>Unlimited</strong> Bandwidth</li>
-                        <li><strong>20GB</strong> Storage</li>
-                        <li>Support Ads</li>
-                        <li>Shared Hosting</li>
-                        <li><strong>24/7</strong> Customer Support</li>
-                    </ul>
                     {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
                     {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
                     <div class="button">
@@ -32,26 +23,19 @@
                 </div>
             </div>
 
-            <div class="plan-box light-green">
+            <div class="plan-box">
                 <div class="plan-options">
                     <div class="plan-price">
-                        <strong>$19</strong>/mo
+                        <strong>Free</strong>
                     </div>
-                    <div class="plan-icon">
-                        <i class="fa fa-briefcase" aria-hidden="true"></i>
-                    </div>
-                    <p class="plan-name">
-                        Premium
-                    </p>
-                    <ul class="plan-details">
-                        <li><strong>FREE</strong> custom domain</li>
-                        <li><strong>Unlimited</strong> email addresses</li>
-                        <li><strong>Unlimited</strong> Bandwidth</li>
-                        <li><strong>100GB</strong> Storage</li>
-                        <li><strong>No Support Ads</strong></li>
-                        <li><strong>24/7</strong> Customer Support</li>
-                    </ul>
 
+                    <div class="plan-icon">
+                        <img src="http://www.smlclebanon.com/Content/uploads/ProductType/439~product1.jpg" alt="">
+                        {{--<i class="fa fa-adjust" aria-hidden="true"></i>--}}
+                    </div>
+                    <div class="plan-name">
+                        Personal
+                    </div>
                     {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
                     {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
                     <div class="button">
@@ -61,28 +45,19 @@
                 </div>
             </div>
 
-            <div class="plan-box hot-pink">
+            <div class="plan-box">
                 <div class="plan-options">
                     <div class="plan-price">
-                        <strong>$50</strong>/mo
+                        <strong>Free</strong>
                     </div>
-                    <div class="plan-icon">
-                        <i class="fa fa-credit-card" aria-hidden="true"></i>
-                    </div>
-                    <p class="plan-name">
-                        Pro
-                    </p>
-                    <ul class="plan-details">
-                        <li><strong>FREE</strong> custom domain</li>
-                        <li><strong>Unlimited</strong> email addresses</li>
-                        <li><strong>Unlimited</strong> Bandwidth</li>
-                        <li><strong>200GB</strong> Storage</li>
-                        <li><strong>No Support Ads</strong></li>
-                        <li><strong>Quick Install</strong> Apps (WordPress, etc)</li>
-                        <li><strong>VPS Hosting</strong></li>
-                        <li><strong>24/7</strong> Customer Support</li>
-                    </ul>
 
+                    <div class="plan-icon">
+                        <img src="http://www.smlclebanon.com/Content/uploads/ProductType/439~product1.jpg" alt="">
+                        {{--<i class="fa fa-adjust" aria-hidden="true"></i>--}}
+                    </div>
+                    <div class="plan-name">
+                        Personal
+                    </div>
                     {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
                     {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
                     <div class="button">
