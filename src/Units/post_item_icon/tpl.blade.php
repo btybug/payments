dcac1b892d0e45c0ded7ea0d3e6573442e5fc243
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-25 13:26:35 +0400

    product item icon ui

---
+++ src/Units/post_item_icon/tpl.blade.php
@@ -0,0 +1,98 @@
+<div class="container">
+    <div class="row">
+        <div class="post-item-icon">
+            <div class="plan-box">
+                <div class="plan-options">
+                    <div class="plan-price">
+                        <strong>Free</strong>
+                    </div>
+
+                    <div class="plan-icon">
+                        <i class="fa fa-adjust" aria-hidden="true"></i>
+                    </div>
+                    <div class="plan-name">
+                        Personal
+                    </div>
+
+                    <ul class="plan-details">
+                        <li>1 sub-domain</li>
+                        <li><strong>10</strong> email addresses</li>
+                        <li><strong>Unlimited</strong> Bandwidth</li>
+                        <li><strong>20GB</strong> Storage</li>
+                        <li>Support Ads</li>
+                        <li>Shared Hosting</li>
+                        <li><strong>24/7</strong> Customer Support</li>
+                    </ul>
+                    {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
+                    {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
+                    <div class="button">
+                        <a class="" href="#">Add To Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                        <a class="" href="#">View Product<i class="fa fa-eye" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="plan-box light-green">
+                <div class="plan-options">
+                    <div class="plan-price">
+                        <strong>$19</strong>/mo
+                    </div>
+                    <div class="plan-icon">
+                        <i class="fa fa-briefcase" aria-hidden="true"></i>
+                    </div>
+                    <p class="plan-name">
+                        Premium
+                    </p>
+                    <ul class="plan-details">
+                        <li><strong>FREE</strong> custom domain</li>
+                        <li><strong>Unlimited</strong> email addresses</li>
+                        <li><strong>Unlimited</strong> Bandwidth</li>
+                        <li><strong>100GB</strong> Storage</li>
+                        <li><strong>No Support Ads</strong></li>
+                        <li><strong>24/7</strong> Customer Support</li>
+                    </ul>
+
+                    {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
+                    {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
+                    <div class="button">
+                        <a class="" href="#">Add To Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                        <a class="" href="#">View Product<i class="fa fa-eye" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="plan-box hot-pink">
+                <div class="plan-options">
+                    <div class="plan-price">
+                        <strong>$50</strong>/mo
+                    </div>
+                    <div class="plan-icon">
+                        <i class="fa fa-credit-card" aria-hidden="true"></i>
+                    </div>
+                    <p class="plan-name">
+                        Pro
+                    </p>
+                    <ul class="plan-details">
+                        <li><strong>FREE</strong> custom domain</li>
+                        <li><strong>Unlimited</strong> email addresses</li>
+                        <li><strong>Unlimited</strong> Bandwidth</li>
+                        <li><strong>200GB</strong> Storage</li>
+                        <li><strong>No Support Ads</strong></li>
+                        <li><strong>Quick Install</strong> Apps (WordPress, etc)</li>
+                        <li><strong>VPS Hosting</strong></li>
+                        <li><strong>24/7</strong> Customer Support</li>
+                    </ul>
+
+                    {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
+                    {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
+                    <div class="button">
+                        <a class="" href="#">Add To Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                        <a class="" href="#">View Product<i class="fa fa-eye" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{!! BBstyle($_this->path."/css/main.css") !!}
+{!!  BBscript($_this->path.'/js/main.js') !!}
