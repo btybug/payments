559f55a9f9b683c4c182fd6354493bc74b2c7271
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-25 10:27:41 +0400

    product light ui

--- src/Units/post_item_light/tpl.blade.php
+++ src/Units/post_item_light/tpl.blade.php
@@ -1,67 +1,85 @@
-<div class="post-item-light">
-    <div class="plan-box">
-        <div class="plan-options">
-            <p class="plan-name">
-                Personal
-            </p>
-            <div class="plan-price">
-                <strong>Free</strong>
+<div class="container">
+    <div class="row">
+        <div class="post-item-light">
+            <div class="plan-box">
+                <div class="plan-options">
+                    <p class="plan-name">
+                        Personal
+                    </p>
+                    <div class="plan-price">
+                        <strong>Free</strong>
+                    </div>
+                    <ul class="plan-details">
+                        <li>1 sub-domain</li>
+                        <li><strong>10</strong> email addresses</li>
+                        <li><strong>Unlimited</strong> Bandwidth</li>
+                        <li><strong>20GB</strong> Storage</li>
+                        <li>Support Ads</li>
+                        <li>Shared Hosting</li>
+                        <li><strong>24/7</strong> Customer Support</li>
+                    </ul>
+                    {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
+                    {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
+                    <div class="button">
+                        <a class="" href="#">Add To Cart</a>
+                        <a class="" href="#">View Product</a>
+                    </div>
+                </div>
             </div>
-            <ul class="plan-details">
-                <li>1 sub-domain</li>
-                <li><strong>10</strong> email addresses</li>
-                <li><strong>Unlimited</strong> Bandwidth</li>
-                <li><strong>20GB</strong> Storage</li>
-                <li>Support Ads</li>
-                <li>Shared Hosting</li>
-                <li><strong>24/7</strong> Customer Support</li>
-            </ul>
 
-            <a class="button" href="#">Choose</a>
-        </div>
-    </div>
+            <div class="plan-box">
+                <div class="plan-options">
+                    <p class="plan-name">
+                        Premium
+                    </p>
+                    <div class="plan-price">
+                        <strong>$19</strong>/mo
+                    </div>
+                    <ul class="plan-details">
+                        <li><strong>FREE</strong> custom domain</li>
+                        <li><strong>Unlimited</strong> email addresses</li>
+                        <li><strong>Unlimited</strong> Bandwidth</li>
+                        <li><strong>100GB</strong> Storage</li>
+                        <li><strong>No Support Ads</strong></li>
+                        <li><strong>24/7</strong> Customer Support</li>
+                    </ul>
 
-    <div class="plan-box">
-        <div class="plan-options">
-            <p class="plan-name">
-                Premium
-            </p>
-            <div class="plan-price">
-                <strong>$19</strong>/mo
+                    {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
+                    {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
+                    <div class="button">
+                        <a class="" href="#">Add To Cart</a>
+                        <a class="" href="#">View Product</a>
+                    </div>
+                </div>
             </div>
-            <ul class="plan-details">
-                <li><strong>FREE</strong> custom domain</li>
-                <li><strong>Unlimited</strong> email addresses</li>
-                <li><strong>Unlimited</strong> Bandwidth</li>
-                <li><strong>100GB</strong> Storage</li>
-                <li><strong>No Support Ads</strong></li>
-                <li><strong>24/7</strong> Customer Support</li>
-            </ul>
 
-            <a class="button" href="#">Choose</a>
-        </div>
-    </div>
+            <div class="plan-box">
+                <div class="plan-options">
+                    <p class="plan-name">
+                        Pro
+                    </p>
+                    <div class="plan-price">
+                        <strong>$50</strong>/mo
+                    </div>
+                    <ul class="plan-details">
+                        <li><strong>FREE</strong> custom domain</li>
+                        <li><strong>Unlimited</strong> email addresses</li>
+                        <li><strong>Unlimited</strong> Bandwidth</li>
+                        <li><strong>200GB</strong> Storage</li>
+                        <li><strong>No Support Ads</strong></li>
+                        <li><strong>Quick Install</strong> Apps (WordPress, etc)</li>
+                        <li><strong>VPS Hosting</strong></li>
+                        <li><strong>24/7</strong> Customer Support</li>
+                    </ul>
 
-    <div class="plan-box">
-        <div class="plan-options">
-            <p class="plan-name">
-                Pro
-            </p>
-            <div class="plan-price">
-                <strong>$50</strong>/mo
+                    {{--<button class="btn select-plan add-to-cart" data-id="{!! $plan["id"] !!}">Add To Cart</button>--}}
+                    {{--<a href="{!! url(get_blog_slug_in_page(),$plan[$col]) !!}" class="btn select-plan">View Product</a>--}}
+                    <div class="button">
+                        <a class="" href="#">Add To Cart</a>
+                        <a class="" href="#">View Product</a>
+                    </div>
+                </div>
             </div>
-            <ul class="plan-details">
-                <li><strong>FREE</strong> custom domain</li>
-                <li><strong>Unlimited</strong> email addresses</li>
-                <li><strong>Unlimited</strong> Bandwidth</li>
-                <li><strong>200GB</strong> Storage</li>
-                <li><strong>No Support Ads</strong></li>
-                <li><strong>Quick Install</strong> Apps (WordPress, etc)</li>
-                <li><strong>VPS Hosting</strong></li>
-                <li><strong>24/7</strong> Customer Support</li>
-            </ul>
-
-            <a class="button" href="#">Choose</a>
         </div>
     </div>
 </div>
