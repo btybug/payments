db15bcfe81082487895b29560fa2fe277f414b78
Sahak <hakobyan.sahak88@gamil.com>
2018-01-25 11:24:24 +0400

    up

--- src/views/settings/general.blade.php
+++ src/views/settings/general.blade.php
@@ -26,6 +26,38 @@
 
                 </div>
             </div>
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab">
+                    <h4 class="panel-title">Tax & services</h4>
+                </div>
+                <div class="panel-body">
+                    {!! Form::model(null,['class' => 'form-horizontal']) !!}
+                    <!-- Multiple Radios -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="radios">Tax options</label>
+                            <div class="col-md-4">
+                                <div class="radio">
+                                    <label for="radios-0">
+                                        <input type="radio" name="radios" id="radios-0" value="1" checked="checked">
+                                        Inserted Price for product INCLUDE Tax
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label for="radios-1">
+                                        <input type="radio" name="radios" id="radios-1" value="2">
+                                        Inserted Price for product EXCLUDE Tax
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                    <div class="form-group">
+                        {!! Form::submit("Save",['class' => 'btn settingBtn pull-right']) !!}
+                    </div>
+                    {!! Form::close() !!}
+
+                </div>
+            </div>
         </div>
     </div>
 @stop
