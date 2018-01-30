d0cd018261c6601ba61adf09b43bfc261e036209
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-30 14:38:02 +0400

    price, tax services unit

---
+++ src/Units/price/tpl.blade.php
@@ -0,0 +1,6 @@
+@php
+    $col = posts_url_manager();
+@endphp
+
+{!! BBstyle($_this->path."/css/main.css") !!}
+{!!  BBscript($_this->path.'/js/main.js') !!}
---
+++ src/Units/tax_services/tpl.blade.php
@@ -0,0 +1,6 @@
+@php
+    $col = posts_url_manager();
+@endphp
+taxxx
+{!! BBstyle($_this->path."/css/main.css") !!}
+{!!  BBscript($_this->path.'/js/main.js') !!}
