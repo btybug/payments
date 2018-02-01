eff3042b567a7f9aabc683c1bd42fd895674e246
Sahak <hakobyan.sahak88@gamil.com>
2018-02-01 13:30:59 +0400

    up

--- src/Units/payment_gateway_buttons/tpl.blade.php
+++ src/Units/payment_gateway_buttons/tpl.blade.php
@@ -19,8 +19,8 @@
             <form action="{!! url('/admin/payments/test-call-back') !!}" method="GET">
                 <script
                         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                        data-key="pk_test_zr3Wfst8jb4GrKU8BcLEUkh9"
-                        data-amount="999"
+                        data-key="{!! Config::get('services.stripe.key') !!}"
+                        data-amount="{!! $cart::total() !!}"
                         data-name="Demo Site"
                         data-description="Widget"
                         data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
