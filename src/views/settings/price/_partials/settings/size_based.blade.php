f03db0ec29edc2598028a3ea3596924114b6499c
Sahak <hakobyan.sahak88@gamil.com>
2018-02-05 16:56:05 +0400

    up

--- src/views/settings/price/_partials/settings/size_based.blade.php
+++ src/views/settings/price/_partials/settings/size_based.blade.php
@@ -2,16 +2,16 @@
 <div class="row">
     <div class="col-md-6">
         <div class="col-md-5"><label class="col-md-4">Length</label>
-            <div class="col-md-8">{!! Form::number('length',null,['class'=>'form-control']) !!}</div>
+            <div class="col-md-8">{!! Form::number('length',null,['class'=>'form-control base-size','min'=>0]) !!}</div>
         </div>
         <div class="col-md-2">X</div>
         <div class="col-md-5"><label class="col-md-4">Height</label>
-            <div class="col-md-8">{!! Form::number('height',null,['class'=>'form-control']) !!}</div>
+            <div class="col-md-8">{!! Form::number('height',null,['class'=>'form-control base-size']) !!}</div>
         </div>
     </div>
     <div class="col-md-6">
         <div class="col-md-3"><label class="col-md-4">Total</label>
-            <div class="col-md-8">{!! Form::number('total',null,['class'=>'form-control']) !!}</div>
+            <div class="col-md-8">{!! Form::text('total',null,['class'=>'form-control base-total','readonly'=>true]) !!}</div>
         </div>
     </div>
     <div>
@@ -21,7 +21,7 @@
                 <div class="col-md-8">
                     <label class="col-mg-4">minimum length</label>
                     <div class="col-md-8">
-                        {!! Form::number('max_length') !!}
+                        {!! Form::number('min_length') !!}
                     </div>
                 </div>
                 <div class="col-md-8">
@@ -38,7 +38,7 @@
                 <div class="col-md-8">
                     <label class="col-mg-4">minimum height</label>
                     <div class="col-md-8">
-                        {!! Form::number('max_height') !!}
+                        {!! Form::number('min_height') !!}
                     </div>
                 </div>
                 <div class="col-md-8">
@@ -53,3 +53,39 @@
     </div>
 </div>
 {!! Form::close() !!}
+@push('javascript')
+    <script>
+        $(function () {
+            var min_length = $('input[name=min_length]').val();
+            $('input[name=length]').attr('min', min_length);
+
+            var min_height = $('input[name=min_height]').val();
+            $('input[name=height]').attr('min', min_height);
+
+            $('input[name=min_height]').on('change', function () {
+                var min_height = $(this).val();
+                if ($('input[name=height]').val() < min_height) {
+                    $('input[name=height]').val(min_height);
+                    $('input[name=height]').change();
+                };
+                $('input[name=height]').attr('min', min_height);
+            });
+
+            $('input[name=min_length]').on('change', function () {
+                var min_length = $(this).val();
+                if ($('input[name=length]').val() < min_length) {
+                    $('input[name=length]').val(min_length);
+                    $('input[name=length]').change();
+                }
+                ;
+                $('input[name=length]').attr('min', min_length);
+            });
+            $('.base-size').on('change', function () {
+                var el1 = $('.base-size')[0];
+                var el2 = $('.base-size')[1];
+                var total = $(el1).val() * $(el2).val();
+                $('.base-total').val(total)
+            });
+        });
+    </script>
+@endpush
