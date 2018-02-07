3dba6c7bedfb68140fb1153982caa22fa1844665
Sahak <hakobyan.sahak88@gamil.com>
2018-02-07 15:04:14 +0400

    update

--- src/Models/Calculator.php
+++ src/Models/Calculator.php
@@ -9,7 +9,27 @@
 namespace BtyBugHook\Payments\Models;
 
 
-class Calculator
+final class Calculator
 {
+    public static function price($jsonData, int $count = 0)
+    {
+        $data = json_decode($jsonData, true);
+        $method = $data['method'];
+        $_this = new self();
+        if (method_exists($_this, $method)) {
+            return call_user_func_array(array($_this, $method), [$count, $data]);
+        }
+    }
+
+    public function quantity_price($count, array $array)
+    {
+        $qtys=$array['qty'];
+        foreach ($qtys as $qty){
+            if($qty['qty']==$count){
+                return $qty['price'];
+            }
+        }
+        throw new \Exception('Invalid count');
+    }
 
 }
