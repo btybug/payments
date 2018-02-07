409c677f408a06b958352e02e8e49ee2b7618f69
Sahak <hakobyan.sahak88@gamil.com>
2018-02-07 17:26:17 +0400

    price html

--- src/Models/PriceHTML.php
+++ src/Models/PriceHTML.php
@@ -11,5 +11,31 @@ namespace BtyBugHook\Payments\Models;
 
 class PriceHTML
 {
+    //{"method":"quantity_price","qty_option":"select","qty":[{"qty":"1","price":"12"},{"qty":"2","price":"19"}]}
+    public static function get($jsonData,$class=null)
+    {
+        $data = json_decode($jsonData, true);
+        $method = $data['method'];
+        $_this = new self();
+        if (method_exists($_this, $method)) {
+            return call_user_func_array(array($_this, $method), [$data,$class]);
+        }
+    }
+
+    public function quantity_price(array $array,$class)
+    {
+        $qty_option=$array['qty_option'];
+        $qtys=$array['qty'];
+        $html='';
+        switch ($qty_option){
+            case 'select':
+                $html.="<select class='$class'>";
+                foreach ($qtys as $qty){
+                    $html.="<option value='".$qty["qty"]."'></option>";
+                }
+                $html.='</select>';
+        }
+       return $html;
+    }
 
 }
