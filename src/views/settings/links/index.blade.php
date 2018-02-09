de2d6e4a300603c4814692e60e47f9b3744ce392
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-09 03:47:15 -0800

    front

--- src/views/settings/links/index.blade.php
+++ src/views/settings/links/index.blade.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: menq
- * Date: 09.02.2018
- * Time: 15:29
- */
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Categories <i class="fa fa-question-circle blue"></i></span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" placeholder="Categories">
+                    <div class="some-content">
+                        <div><i class="fa fa-minus-circle"></i> Desktops</div>
+                        <div><i class="fa fa-minus-circle"></i> Cameras</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Tags <i class="fa fa-question-circle blue"></i></span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" placeholder="Tags">
+                    <div class="some-content"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    .line-height{
+        line-height:35px;
+    }
+    .custom_margin{
+        margin: 5px 0px;
+    }
+    .blue{
+        color:#0000ff;
+    }
+    .some-content{
+        min-height:200px;
+        background-color:#cccccc3d;
+        padding:10px;
+        border-radius: 4px;
+    }
+</style>
