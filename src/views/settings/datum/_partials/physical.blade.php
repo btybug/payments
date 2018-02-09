5308643bddb6b91a5c3950c7204e812ab93ba4a2
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-09 02:28:51 -0800

    front

--- src/views/settings/datum/_partials/physical.blade.php
+++ src/views/settings/datum/_partials/physical.blade.php
@@ -1,1 +1,103 @@
-digital
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Dimensions (L x W x H)</span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-4">
+                    <input type="text" class="form-control" placeholder="0.0000000000">
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" placeholder="0.0000000000">
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" placeholder="0.0000000000">
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Weight</span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" placeholder="0.0000000000">
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">SKU <i class="fa fa-question-circle blue"></i></span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" placeholder="SKU">
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Location</span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" placeholder="Location">
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Quantity</span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <input type="number" class="form-control" placeholder="0" min="0">
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Subtrack Stock</span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <select class="form-control">
+                        <option value="1">Yes</option>
+                        <option value="0">NO</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-md-12 custom_margin form-group">
+            <div class="col-xs-2">
+                <span class="line-height pull-right">Out Of Stock Status <i class="fa fa-question-circle blue"></i></span>
+            </div>
+            <div class="col-xs-10 form-group">
+                <div class="col-md-12">
+                    <select class="form-control">
+                        <option value="1">2-3 Days</option>
+                        <option value="0">5-6 Months</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    .line-height{
+        line-height:35px;
+    }
+    .custom_margin{
+        margin: 5px 0px;
+    }
+    .blue{
+        color:#0000ff;
+    }
+</style>
