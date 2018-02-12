ddfb51f6e0b733573a88fef86126adc101831aea
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-12 15:56:53 +0400

    pym_shipping ui

---
+++ src/Units/pym_shipping/tpl.blade.php
@@ -0,0 +1,11 @@
+<div class="shipping-calc">
+    <div class="title">
+        Shipping Calculator
+    </div>
+    <div class="content">
+
+    </div>
+
+</div>
+{!! BBstyle($_this->path."/css/main.css") !!}
+{!!  BBscript($_this->path.'/js/main.js') !!}
