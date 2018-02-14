bbc4888f46ce8b278f0db8108e1c3dc29040e4c2
terakyan <edo.terakyan@gmail.com>
2018-02-14 02:43:50 -0800

    shopping

--- src/Units/shopping_cart/tpl.blade.php
+++ src/Units/shopping_cart/tpl.blade.php
@@ -2,83 +2,103 @@
     use Gloudemans\Shoppingcart\Facades\Cart;
 @endphp
 
-<div class="container">
+<div class="container" style="color: black;">
     <div class="row">
         <h1>Shopping Cart</h1>
-
-        <div class="shopping-cart">
-            <div class="column-labels">
-                <label class="product-image">Image</label>
-                <label class="product-details">Product</label>
-                <label class="product-price">Price</label>
-                <label class="product-discount">Discount</label>
-                <label class="product-vat">VAT</label>
-                <label class="product-quantity">Quantity</label>
-                <label class="product-line-price">Total</label>
-                <label class="product-removal">Remove</label>
-            </div>
-            @if(Cart::count())
-
-                @foreach(Cart::content() as $item)
-                    <div class="product">
-                        <div class="product-image">
-                            <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg" alt=""
-                                 class="{{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : ''}}">
-                        </div>
-                        <div class="product-details">
-                            <div class="product-title">
-                                <p class="{{isset($settings['option_2_container_item_style']) ? $settings['option_2_container_item_style'] : ''}} {{isset($settings['option_3_container_item_style']) ? $settings['option_3_container_item_style'] : ''}}">
-                                    {!! $item->name !!}
-                                </p>
-                            </div>
-                            <p class="product-description {{isset($settings['option_4_container_item_style']) ? $settings['option_4_container_item_style'] : ''}} {{isset($settings['option_5_container_item_style']) ? $settings['option_5_container_item_style'] : ''}}">
-                                The best dog bones of all time. Holy crap. Your dog will be begging
-                                for these things! I got curious once and ate one myself. I'm a fan.</p>
-                        </div>
-                        <div class="product-price {{isset($settings['option_6_container_item_style']) ? $settings['option_6_container_item_style'] : ''}} {{isset($settings['option_7_container_item_style']) ? $settings['option_7_container_item_style'] : ''}}">
-                            {!! $item->price !!}
-                        </div>
-                        <div class="product-discount {{isset($settings['option_6_container_item_style']) ? $settings['option_6_container_item_style'] : ''}} {{isset($settings['option_7_container_item_style']) ? $settings['option_7_container_item_style'] : ''}}">
-                            0%
-                        </div>
-                        <div class="product-vat {{isset($settings['option_6_container_item_style']) ? $settings['option_6_container_item_style'] : ''}} {{isset($settings['option_7_container_item_style']) ? $settings['option_7_container_item_style'] : ''}}">
-                            {!! (isset($item->options['vat']['amount']))?$item->options['vat']['amount']:0 !!}
-                        </div>
-                        <div class="product-quantity">
-                            <input type="number" value="{!! $item->qty !!}" min="1">
-                        </div>
-                        @php
-                        $price=isset($item->options['vat']['price'])?$item->options['vat']['price']:$item->price;
-                        @endphp
-
-                        <div class="product-line-price">{!! $item->qty *$price  !!}</div>
-                        <div class="product-removal">
-                            <button class="{{isset($settings['option_8_container_item_style']) ? $settings['option_8_container_item_style'] : ''}} {{isset($settings['option_9_container_item_style']) ? $settings['option_9_container_item_style'] : ''}}">
-                                Remove
-                            </button>
-                        </div>
-                    </div>
-                @endforeach
-            @endif
-
-            <div class="totals">
-                <div class="col-md-6">
-                    <div class="col-md-12">
-                        {{isset($settings['shipping']) ? BBRenderUnits($settings['shipping']) : ''}}
-                    </div>
-                    <div class="col-md-12">
-                        {!! isset($settings['pym_shipping']) ? BBRenderUnits($settings['pym_shipping']) : ''!!}
-                    </div>
+        <div class="table-responsive product">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Discount</th>
+                    <th>VAT</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Remove</th>
+                </tr>
+                </thead>
+                <tbody>
+                @if(Cart::count())
+                    @foreach(Cart::content() as $item)
+                        <tr>
+                            <td>
+                                <div class="product-cart-image">
+                                    <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg" alt=""
+                                         class="{{isset($settings['option_1_container_item_style']) ? $settings['option_1_container_item_style'] : ''}}">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="product-cart-details">
+                                    <div class="product-title">
+                                        <p class=" ">
+                                            {!! $item->name !!}
+                                        </p>
+                                    </div>
+                                    <p class="product-description  ">
+                                        The best dog bones of all time. Holy crap. Your dog will be begging
+                                        for these things!</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="product-cart-price  ">
+                                    {!! $item->price !!}
+                                </div>
+                            </td>
+                            <td>
+                                <div class="product-cart-discount  ">
+                                    0%
+                                </div>
+                            </td>
+                            <td>
+                                <div class="product-cart-vat  ">
+                                    {!! (isset($item->options['vat']['amount']))?$item->options['vat']['amount']:0 !!}
+                                </div>
+                            </td>
+                            <td>
+                                <div class="product-cart-quantity">
+                                    <input type="number" value="{!! $item->qty !!}" min="1">
+                                </div>
+                            </td>
+                            <td>
+                                @php
+                                    $price=isset($item->options['vat']['price'])?$item->options['vat']['price']:$item->price;
+                                @endphp
+                                <div class="product-cart-line-price">{!! $item->qty *$price  !!}</div>
+                            </td>
+                            <td>
+                                <div class="product-cart-removal">
+                                    <button class="btn btn-danger">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </div>
+        <div class="totals">
+            <div class="col-md-9">
+                <div class="row">
 
                 </div>
-                <div class="col-md-6">
+                <div class="row">
+                    {!! isset($settings['pym_shipping']) ? BBRenderUnits($settings['pym_shipping']) : ''!!}
+                </div>
+
+            </div>
+            <div class="col-md-3">
+                <div class="total-all-item">
                     <div class="totals-item">
                         <label>Subtotal</label>
-                        <div class="totals-value" id="cart-subtotal">{!! Cart::subtotal() !!}</div>
+                        <div class="totals-value" id="cart-subtotal">100.00</div>
                     </div>
                     <div class="totals-item">
                         <label>Tax (5%)</label>
-                        <div class="totals-value" id="cart-tax">{!! Cart::tax() !!}</div>
+                        <div class="totals-value" id="cart-tax">21.00</div>
                     </div>
                     <div class="totals-item">
                         <label>Voucher </label>
@@ -90,16 +110,14 @@
                     </div>
                     <div class="totals-item totals-item-total">
                         <label>Grand Total</label>
-                        <div class="totals-value" id="cart-total">{!! Cart::total() !!}</div>
+                        <div class="totals-value" id="cart-total">121.00</div>
                     </div>
+                    <div class="clearfix"></div>
+                    <a href="/check-out" class="checkout  ">Checkout</a>
                 </div>
             </div>
-
-            <a href="{!! url('/check-out') !!}"
-               class="checkout {{isset($settings['option_10_container_item_style']) ? $settings['option_10_container_item_style'] : ''}} {{isset($settings['option_11_container_item_style']) ? $settings['option_11_container_item_style'] : ''}}">Checkout</a>
-
+            <div class="clearfix"></div>
         </div>
-
     </div>
 </div>
 
