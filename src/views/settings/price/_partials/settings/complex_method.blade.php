1f393a32ec9e9a37a3209d4f84ecd3ff08b198ac
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-14 21:54:55 +0400

    up

--- src/views/settings/price/_partials/settings/complex_method.blade.php
+++ src/views/settings/price/_partials/settings/complex_method.blade.php
@@ -8,7 +8,6 @@
         <div class="col-md-12 m-t-15" style="    margin-bottom: 9px;
     margin-top: 7px;">
             <button class="btn btn-success pull-right" >Get Attributes
-                attribute
             </button>
         </div>
         <div class="col-md-4"></div>
