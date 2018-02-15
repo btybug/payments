153edd975fff5731c15c47901dc9c72dc7c54f00
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-15 01:34:28 -0800

    dd

--- src/Http/Controllers/IndexConroller.php
+++ src/Http/Controllers/IndexConroller.php
@@ -89,6 +89,7 @@ class IndexConroller extends Controller
         return redirect()->route('payments_sopping_cart_zones');
     }
     public function getZones(Request $request){
+        dd($request->all());
         $country_id = $request->id;
         $arr = [];
         $states = collect(json_decode(\File::get(plugins_path('vendor'.DS.'sahak.avatar'.DS.'payments'.DS.'src'.DS.'views'.DS.'shopping'.DS.'zones_dir'.DS.'state.json')),true))
