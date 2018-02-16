77025ca9bd10d106e81b789b908ad3400e05475b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-16 15:42:17 +0400

    orders history

---
+++ src/Units/orders_history/tpl.blade.php
@@ -0,0 +1,40 @@
+<div class="order_history">
+    <h3>Order history</h3>
+    <table class="bty-table bty-table-active bty-table-hover">
+        <thead>
+        <tr>
+            <th>Order ID</th>
+            <th>Customer</th>
+            <th>No. of Products</th>
+            <th>Status</th>
+            <th>Total</th>
+            <th>Date Added</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>#88</td>
+            <td>User name</td>
+            <td>2</td>
+            <td><span class="td-inactive">Pending</span></td>
+            <td>$130</td>
+            <td>16/02/18</td>
+            <td><a href="#" class="btn btn-danger"><i class="fa fa-eye"></i></a></td>
+        </tr>
+        <tr>
+            <td>#8</td>
+            <td>User name</td>
+            <td>1</td>
+            <td><span class="td-active">Sending</span></td>
+            <td>$30</td>
+            <td>01/02/18</td>
+            <td><a href="#" class="btn btn-danger"><i class="fa fa-eye"></i></a></td>
+        </tr>
+        </tbody>
+    </table>
+    <div class="buttons">
+        <div class="pull-right"><a href="" class="btn btn-primary">Continue</a></div>
+    </div>
+</div>
+{!! BBstyle($_this->path."/css/main.css") !!}
