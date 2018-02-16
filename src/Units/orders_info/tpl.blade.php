0eecc977464f8e4ec278bd68f257f991e1a914a4
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-16 16:27:46 +0400

    orders info

---
+++ src/Units/orders_info/tpl.blade.php
@@ -0,0 +1,138 @@
+<div class="order_info">
+    <h3>Order info</h3>
+
+    <table class="bty-table bty-table-hover detail">
+        <thead>
+        <tr>
+            <td class="text-left" colspan="2">Order Details</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="left"><strong>Order ID: #%s</strong> #8<br>
+                <strong>Date Added:</strong> 16/02/2018
+            </td>
+            <td class="right"><strong>Payment Method:</strong> Cash On Delivery<br>
+                <strong>Shipping Method:</strong> Flat Shipping Rate
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <table class="bty-table bty-table-hover address">
+        <thead>
+        <tr>
+            <td class="left">Payment Address</td>
+            <td class="right">Shipping Address</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="text-left">dsv fd vdfv<br>dfvfdvdf<br>hvb<br>hjb<br>dcdcdc vdcdc<br>Suffolk<br>United Kingdom
+            </td>
+            <td class="text-left">dsv fd vdfv<br>dfvfdvdf<br>hvb<br>hjb<br>dcdcdc vdcdc<br>Suffolk<br>United Kingdom
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <div class="table-responsive">
+        <table class="bty-table bty-table-hover name">
+            <thead>
+            <tr>
+                <td class="text-left">Product Name</td>
+                <td class="text-left">Model</td>
+                <td class="text-right">Quantity</td>
+                <td class="text-right">Price</td>
+                <td class="text-right">Total</td>
+                <td></td>
+            </tr>
+            </thead>
+            <tbody>
+
+            <tr>
+                <td class="text-left">iMac
+                </td>
+                <td class="text-left">Product 14</td>
+                <td class="text-right">1</td>
+                <td class="text-right">$122.00</td>
+                <td class="text-right">$122.00</td>
+                <td class="text-right button">
+                    <a href="" class="btn btn-primary"><i class="fa fa-shopping-cart"></i></a>
+                    <a href="" class="btn btn-danger"><i class="fa fa-reply"></i></a>
+                </td>
+            </tr>
+            </tbody>
+
+            <tfoot>
+
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-right"><b>Sub-Total</b></td>
+                <td class="text-right">$100.00</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-right"><b>Flat Shipping Rate</b></td>
+                <td class="text-right">$5.00</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-right"><b>Eco Tax (-2.00)</b></td>
+                <td class="text-right">$4.00</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-right"><b>VAT (20%)</b></td>
+                <td class="text-right">$21.00</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-right"><b>Total</b></td>
+                <td class="text-right">$130.00</td>
+                <td></td>
+            </tr>
+            </tfoot>
+
+        </table>
+    </div>
+    <table class="bty-table bty-table-hover comment">
+        <thead>
+        <tr>
+            <td class="text-left">Order Comments</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="text-left">cdcdcd</td>
+        </tr>
+        </tbody>
+    </table>
+    <h3>Order History</h3>
+    <table class="bty-table bty-table-active bty-table-hover">
+        <thead>
+        <tr>
+            <td class="text-left">Date Added</td>
+            <td class="text-left">Status</td>
+            <td class="text-left">Comment</td>
+        </tr>
+        </thead>
+        <tbody>
+
+        <tr>
+            <td class="text-left">16/02/2018</td>
+            <td class="text-left"><span class="td-inactive">Pending</span></td>
+            <td class="text-left"></td>
+        </tr>
+        </tbody>
+
+    </table>
+
+
+    <div class="buttons">
+        <div class="pull-right"><a href="" class="btn btn-primary">Continue</a></div>
+    </div>
+</div>
+{!! BBstyle($_this->path."/css/main.css") !!}
