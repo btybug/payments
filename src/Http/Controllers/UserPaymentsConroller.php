6c7c1b6a5ce1b0214d038271fd9e7060b8969a49
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-16 04:51:40 -0800

    fix

--- src/Http/Controllers/UserPaymentsConroller.php
+++ src/Http/Controllers/UserPaymentsConroller.php
@@ -40,7 +40,6 @@ class UserPaymentsConroller extends Controller
     }
 
     public function editShippingAddressSave(Request $request,$key){
-        dd($request->all());
         $data = json_decode(auth()->user()->shipping_address,true);
         $data[$key] = $request->except('_token');
         $user = User::where("id",auth()->user()->id)->first();
